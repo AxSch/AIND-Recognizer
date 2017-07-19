866ec896081b754c25bc4e9730cc75c0f44952c9
Alexander Schuneman <alex_schuneman@outlook.com>
2017-07-19 02:03:48 +0100

    Part 3: Recognizer Added: Recognizer, pulling everything together

--- my_recognizer.py
+++ my_recognizer.py
@@ -22,4 +22,30 @@ def recognize(models: dict, test_set: SinglesData):
     guesses = []
     # TODO implement the recognizer
     # return probabilities, guesses
-    raise NotImplementedError
+    for word_id in range(test_set.num_items):  # iterate through the test_set
+        X, Xlength = test_set.get_item_Xlengths(word_id)  # get the length of the word
+        logL_dict = {} # Likelihood dict
+        selected_word = None
+        best_score = float('-inf')
+
+        # iterate through the models
+        for word, model in models.items():
+            try:
+                # calculate logL of the given word
+                logL_dict[word] = model.score(X, Xlength)
+
+            except:
+
+                logL_dict[word] = float('-inf')
+
+            # handles maximum score for given word
+            if logL_dict[word] > best_score:
+                best_score = logL_dict[word]
+                selected_word = word
+        # Update probabilities with the Likelihood score for given word's model
+        probabilities.append(logL_dict)
+
+        # Update guesses with the selected model for given word
+        guesses.append(selected_word)
+
+    return probabilities, guesses
